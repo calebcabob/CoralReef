163fb4929661af21d3002416e3517c2fdf73d067
ezio84 <brabus84@gmail.com>
2017-09-10 00:32:52 -0400

    Battery light customization [2/2]
    AquariOS: utilizing our new color picker with this
    
    aex edits: Add back disable battery light toggle
    for obvious reason, also set other options dependencies
    on it
    
    Signed-off-by: calebcabob <calphonic@gmail.com>

--- src/com/aquarios/coralreef/fragments/LedNotifications.java
+++ src/com/aquarios/coralreef/fragments/LedNotifications.java
@@ -16,7 +16,10 @@
 
 package com.aquarios.coralreef.fragments;
 
+import android.content.ContentResolver;
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.os.UserHandle;
 import android.support.v7.preference.PreferenceCategory;
 import android.support.v7.preference.ListPreference;
 import android.support.v7.preference.Preference;
@@ -30,17 +33,108 @@ import com.android.settings.SettingsPreferenceFragment;
 import com.android.settings.Utils;
 
 import com.android.internal.logging.nano.MetricsProto;
+import com.android.settings.aquarios.SystemSettingSwitchPreference;
+import com.jaredrummler.android.colorpicker.ColorPreference;
 
 public class LedNotifications extends SettingsPreferenceFragment implements Preference.OnPreferenceChangeListener {
+
+    private ColorPickerPreference mLowColor;
+    private ColorPickerPreference mMediumColor;
+    private ColorPickerPreference mFullColor;
+    private ColorPickerPreference mReallyFullColor;
+    private SystemSettingSwitchPreference mLowBatteryBlinking;
+
+    private PreferenceCategory mColorCategory;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.led_notifications);
+
+
+        PreferenceScreen prefSet = getPreferenceScreen();
+        mColorCategory = (PreferenceCategory) findPreference("battery_light_cat");
+
+        mLowBatteryBlinking = (SystemSettingSwitchPreference)prefSet.findPreference("battery_light_low_blinking");
+        if (getResources().getBoolean(
+                        com.android.internal.R.bool.config_ledCanPulse)) {
+            mLowBatteryBlinking.setChecked(Settings.System.getIntForUser(getContentResolver(),
+                            Settings.System.BATTERY_LIGHT_LOW_BLINKING, 0, UserHandle.USER_CURRENT) == 1);
+            mLowBatteryBlinking.setOnPreferenceChangeListener(this);
+        } else {
+            prefSet.removePreference(mLowBatteryBlinking);
+        }
+
+        if (getResources().getBoolean(com.android.internal.R.bool.config_multiColorBatteryLed)) {
+            int color = Settings.System.getIntForUser(getContentResolver(),
+                    Settings.System.BATTERY_LIGHT_LOW_COLOR, 0xFFFF0000,
+                            UserHandle.USER_CURRENT);
+            mLowColor = (ColorPickerPreference) findPreference("battery_light_low_color");
+            mLowColor.setAlphaSliderEnabled(true);
+            mLowColor.setNewPreviewColor(color);
+            mLowColor.setOnPreferenceChangeListener(this);
+
+            color = Settings.System.getIntForUser(getContentResolver(),
+                    Settings.System.BATTERY_LIGHT_MEDIUM_COLOR, 0xFFFFFF00,
+                            UserHandle.USER_CURRENT);
+            mMediumColor = (ColorPickerPreference) findPreference("battery_light_medium_color");
+            mMediumColor.setAlphaSliderEnabled(true);
+            mMediumColor.setNewPreviewColor(color);
+            mMediumColor.setOnPreferenceChangeListener(this);
+
+            color = Settings.System.getIntForUser(getContentResolver(),
+                    Settings.System.BATTERY_LIGHT_FULL_COLOR, 0xFF00FF00,
+                            UserHandle.USER_CURRENT);
+            mFullColor = (ColorPickerPreference) findPreference("battery_light_full_color");
+            mFullColor.setAlphaSliderEnabled(true);
+            mFullColor.setNewPreviewColor(color);
+            mFullColor.setOnPreferenceChangeListener(this);
+
+            color = Settings.System.getIntForUser(getContentResolver(),
+                    Settings.System.BATTERY_LIGHT_REALLYFULL_COLOR, 0xFF00FF00,
+                            UserHandle.USER_CURRENT);
+            mReallyFullColor = (ColorPickerPreference) findPreference("battery_light_reallyfull_color");
+            mReallyFullColor.setAlphaSliderEnabled(true);
+            mReallyFullColor.setNewPreviewColor(color);
+            mReallyFullColor.setOnPreferenceChangeListener(this);
+        } else {
+            prefSet.removePreference(mColorCategory);
+        }
     }
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (preference.equals(mLowColor)) {
+            int color = ((Integer) newValue).intValue();
+            Settings.System.putIntForUser(getContentResolver(),
+                    Settings.System.BATTERY_LIGHT_LOW_COLOR, color,
+                    UserHandle.USER_CURRENT);
+            return true;
+        } else if (preference.equals(mMediumColor)) {
+            int color = ((Integer) newValue).intValue();
+            Settings.System.putIntForUser(getContentResolver(),
+                    Settings.System.BATTERY_LIGHT_MEDIUM_COLOR, color,
+                    UserHandle.USER_CURRENT);
+            return true;
+        } else if (preference.equals(mFullColor)) {
+            int color = ((Integer) newValue).intValue();
+            Settings.System.putIntForUser(getContentResolver(),
+                    Settings.System.BATTERY_LIGHT_FULL_COLOR, color,
+                    UserHandle.USER_CURRENT);
+            return true;
+        } else if (preference.equals(mReallyFullColor)) {
+            int color = ((Integer) newValue).intValue();
+            Settings.System.putIntForUser(getContentResolver(),
+                    Settings.System.BATTERY_LIGHT_REALLYFULL_COLOR, color,
+                    UserHandle.USER_CURRENT);
+            return true;
+        } else if (preference == mLowBatteryBlinking) {
+            boolean value = (Boolean) newValue;
+            Settings.System.putIntForUser(getActivity().getContentResolver(),
+                    Settings.System.BATTERY_LIGHT_LOW_BLINKING, value ? 1 : 0,
+                    UserHandle.USER_CURRENT);
+            mLowBatteryBlinking.setChecked(value);
+        }
         return false;
     }
 
